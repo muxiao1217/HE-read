974eaa0298f48e5102b1e02a7a69ef624a248455
muxiao1217 <360089592@qq.com>
2018-01-12 11:26:23 +0800

    注释代码

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def read():
     camera = cv2.VideoCapture(0)
     success, frame = camera.read()
     baidu = Baidu()
-    frame = cv2.imread('C:/project/code/HE-read/src/1.jpg')
+    # frame = cv2.imread('C:/project/code/HE-read/src/1.jpg')
     baidu.read_image_text(frame)
     camera.release()
 
